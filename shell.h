36d7cbbf0ad7feca6f54d3bf61963eaeea00c93f
daolao <dalao1002@gmail.com>
2018-11-03 23:41:04 +0100

    add amp&

--- shell.h
+++ shell.h
@@ -16,7 +16,7 @@
 #define BUFFER_SIZE 4096
 #define ERR_FILE(FILE) "fichero %s: Error %s. Descripcion del error\n", FILE, strerror(errno)
 #define ERR_COMMAND "mandato: No se encuentra el mandato \s \s\n"
-#define JOBINFO "[%d]+ Running \t %s\n"
+#define JOBINFO "[%d]+ Running \t %s &\n"
 #define DEFAULT_FILE_CREATE_MODE 0666
 
 typedef struct JobInfo {
